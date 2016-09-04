8c3dd48063c6a7f92aa790e294875e021e11e3d6
Rangel Reale <rangelspam@gmail.com>
2016-09-04 11:52:14 -0300

    * Video convert options

--- convert.go
+++ convert.go
@@ -6,10 +6,24 @@ import (
 	"strings"
 )
 
-func (i *Info) Convert(filename string, outfilename string, width int, height int) error {
-	// letterbox
-	// http://superuser.com/questions/547296/resizing-videos-with-ffmpeg-avconv-to-fit-into-static-sized-player
-	sscale := `scale=iw*min($width/iw\,$height/ih):ih*min($width/iw\,$height/ih), pad=$width:$height:($width-iw*min($width/iw\,$height/ih))/2:($height-ih*min($width/iw\,$height/ih))/2`
+type Convert_ScaleMode int
+
+const (
+	CScaleMode_LetterBox Convert_ScaleMode = 1
+	CScaleMode_Fit                         = 2
+)
+
+func (i *Info) Convert(filename string, outfilename string, width int, height int, scalemode Convert_ScaleMode,
+	moreargs []string) error {
+	sscale := ""
+	if scalemode == CScaleMode_LetterBox {
+		// letterbox
+		// http://superuser.com/questions/547296/resizing-videos-with-ffmpeg-avconv-to-fit-into-static-sized-player
+		sscale = `scale=iw*min($width/iw\,$height/ih):ih*min($width/iw\,$height/ih), pad=$width:$height:($width-iw*min($width/iw\,$height/ih))/2:($height-ih*min($width/iw\,$height/ih))/2`
+	} else {
+		// https://trac.ffmpeg.org/wiki/Scaling%20(resizing)%20with%20ffmpeg
+		sscale = `scale=w=$width:h=$width:force_original_aspect_ratio=decrease`
+	}
 	sscale = strings.Replace(sscale, "$width", strconv.FormatInt(int64(width), 10), -1)
 	sscale = strings.Replace(sscale, "$height", strconv.FormatInt(int64(height), 10), -1)
 
@@ -20,8 +34,11 @@ func (i *Info) Convert(filename string, outfilename string, width int, height in
 		filename,
 		"-filter:v",
 		sscale,
-		outfilename,
 	}
+	if moreargs != nil {
+		args = append(args, moreargs...)
+	}
+	args = append(args, outfilename)
 	cmd := exec.Command(i.FFMpeg.exe_ffmpeg, args...)
 
 	err := i.execCmd(cmd)
